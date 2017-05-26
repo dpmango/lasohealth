9d360493193952e3ac9d16494ea4d2bf64e77d23
dpmango <xs290@me.com>
2017-05-26 21:14:26 +0300

    gulp boolerplate

---
+++ src/js/es2015/main.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+
+  const _window = $(window);
+  const _document = $(document);
+
+ 	// Prevent # behavior
+	$('[href="#"]').click((e) => {
+		e.preventDefault();
+	});
+
+	// Smoth scroll
+	$('a[href^="#section"]').click(() => {
+        var el = $(this).attr('href');
+        $('body, html').animate({
+            scrollTop: $(el).offset().top}, 1000);
+        return false;
+	});
+
+  // owl
+  $('#owlFirst').owlCarousel({
+    loop: true,
+    nav: true,
+    margin: 0,
+    responsive: {
+      0:{
+        items: 1,
+      },
+      600:{
+        items: 1,
+      },
+      1000:{
+        items: 1,
+      }
+    }
+  });
+
+  // Magnific Popup
+  var startWindowScroll = 0;
+  $('.popup-with-zoom-anim').magnificPopup({
+    type: 'inline',
+    fixedContentPos: true,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-zoom-in',
+    callbacks: {
+      beforeOpen: function() {
+        startWindowScroll = _window.scrollTop();
+        $('html').addClass('mfp-helper');
+      },
+      close: function() {
+        $('html').removeClass('mfp-helper');
+        _window.scrollTop(startWindowScroll);
+      }
+    }
+  });
+
+  $('.popup-with-move-anim').magnificPopup({
+    type: 'inline',
+    fixedContentPos: false,
+    fixedBgPos: true,
+    overflowY: 'auto',
+    closeBtnInside: true,
+    preloader: false,
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-slide-bottom'
+  });
+
+  $('.popup-gallery').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1]
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+		}
+	});
+
+  // Masked input
+  $("#date").mask("99/99/9999",{placeholder:"mm/dd/yyyy"});
+  $("input[name='phone']").mask("9 (999) 999-9999");
+  $("#tin").mask("99-9999999");
+  $("#ssn").mask("999-99-9999");
+
+});
